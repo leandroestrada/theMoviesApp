cace35de90723433cbcc51415a0a5ab1747898c4
leandroestrada1 <leandro.estrada@itsgroup.com.br>
2019-11-23 22:42:21 -0300

    Adding search bar and testing the network request

---
+++ theMovieDbApp/Views/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  theMovieDbApp
+//
+//  Created by leandro de araujo estrada on 23/11/19.
+//  Copyright © 2019 Leandro Estrada. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
