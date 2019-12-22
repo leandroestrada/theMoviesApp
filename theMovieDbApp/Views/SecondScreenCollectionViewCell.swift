a01c6b05d92e729b35d7343e7a9e8fe70bfab73d
leandroestrada1 <leandro.a.estrada@gmail.com>
2019-12-22 09:57:23 -0300

    Adding collectionView for second screen and fixing layout issues

---
+++ theMovieDbApp/Views/SecondScreenCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SecondScreenCollectionViewCell.swift
+//  theMovieDbApp
+//
+//  Created by leandro de araujo estrada on 20/12/19.
+//  Copyright © 2019 Leandro Estrada. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    
+    
+}
